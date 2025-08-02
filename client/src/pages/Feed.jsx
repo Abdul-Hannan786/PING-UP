7948f4948dfead78201fdf5ef15a33ad09128759
Abdul Hannan <abdulhannanhere@gmail.com>
2025-08-03 03:56:53 +0500

    Update in the feed page

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { dummyPostsData } from "../assets/assets";
+import Loading from "../components/Loading";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default Feed
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+    setLoading(false);
+  };
+  console.log(feeds);
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
+      {/* Stories and post list */}
+      <div className="p-4 space-y-6">
+        <h1>Stories here</h1>
+        <div>List of posts</div>
+      </div>
+
+      {/* Right Sidebar */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+        <h1>Recent messages</h1>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Feed;
