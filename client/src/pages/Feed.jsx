afb84f6a9bf2c7d16f3cda75761ec9755888bf22
Abdul Hannan <abdulhannanhere@gmail.com>
2025-08-05 00:17:35 +0500

    Post List created

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import StoriesBar from "../components/StoriesBar";
+import PostCard from "../components/PostCard";
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -19,9 +20,13 @@ const Feed = () => {
   return !loading ? (
     <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
       {/* Stories and post list */}
-      <div className="p-4 space-y-6">
+      <div>
         <StoriesBar />
-        <div>List of posts</div>
+        <div className="p-4 space-y-6">
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
       </div>
 
       {/* Right Sidebar */}
