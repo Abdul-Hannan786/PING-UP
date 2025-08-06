063e189e397ddb6a9e061000ab2a374a72ade1dc
Abdul Hannan <abdulhannanhere@gmail.com>
2025-08-07 03:22:15 +0500

    Create Post page completed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { dummyUserData } from "../assets/assets";
-import { X } from "lucide-react";
+import { Image, X } from "lucide-react";
+import toast from "react-hot-toast";
 
 const CreatePost = () => {
   const [content, setContent] = useState("");
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
   const user = dummyUserData;
+
+  const handleSubmit = async () => {};
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-slate-50 to-white">
@@ -66,7 +69,36 @@ const CreatePost = () => {
           )}
 
           {/* Bottom Bar */}
-          <div></div>
+          <div className="flex items-center justify-between pt-3 border-t border-gray-300">
+            <label
+              htmlFor="images"
+              className="flex items-center gap-2 text-sm text-gray-500 hover:text-gray-700 transition cursor-pointer"
+            >
+              <Image className="size-6" />
+            </label>
+
+            <input
+              type="file"
+              id="images"
+              accept="image/*"
+              hidden
+              multiple
+              onChange={(e) => setImages([...images, ...e.target.files])}
+            />
+            <button
+              disabled={loading}
+              onClick={() =>
+                toast.promise(handleSubmit(), {
+                  loading: "Uploading...",
+                  success: <p>Post Uploaded</p>,
+                  error: <p>Failed to Upload</p>,
+                })
+              }
+              className="text-sm bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white font-medium px-8 py-2 rounded-md cursor-pointer"
+            >
+              Publish Post
+            </button>
+          </div>
         </div>
       </div>
     </div>
