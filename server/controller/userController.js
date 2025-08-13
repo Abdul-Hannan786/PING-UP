ba9e28f8ffb57bc293db11fe0e3ed98da0895228
Abdul Hannan <abdulhannanhere@gmail.com>
2025-08-14 03:31:53 +0500

    User controllers completed

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -113,3 +113,52 @@ export const discoverUsers = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// Function to follow a user
+export const followUser = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+
+    const user = await User.findById(userId);
+    if (user.following.includes(id)) {
+      return res.json({
+        success: false,
+        message: "You are already following this user",
+      });
+    }
+
+    user.following.push(id);
+    await user.save();
+
+    const toUser = await User.findById(id);
+    toUser.followers.push(userId);
+    await toUser.save();
+
+    res.json({ success: true, message: "Now you are following this user" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Function to unfollow a user
+export const unfollowUser = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { id } = req.body;
+
+    const user = await User.findById(userId);
+    user.following = user.following.filter((user) => user !== id);
+    await user.save();
+
+    const toUser = await User.findById(id);
+    toUser.followers = toUser.followers.filter((user) => user !== userId);
+    await toUser.save();
+
+    res.json({ success: true, message: "Now you are following this user" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
