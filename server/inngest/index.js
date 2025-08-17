923fac139f95e892c8b4a3a6423e6fb235749be1
Abdul Hannan <abdulhannanhere@gmail.com>
2025-08-17 05:13:35 +0500

    Email sending pending

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Connection from "../models/Connection.js";
+import sendEmail from "../configs/nodemailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -56,6 +58,44 @@ const syncUserDeletion = inngest.createFunction(
   async ({ event }) => {
     const { id } = event.data;
     await User.findByIdAndDelete(id);
+  }
+);
+
+// Inngest function to send reminder when a new connection request is added
+const sendNewConnectionRequestReminder = inngest.createFunction(
+  { id: "send-new-connection-request-reminder" },
+  { event: "app/connection-request" },
+
+  async ({ event, step }) => {
+    const { connectionId } = event.data;
+
+    await step.run("send-connection-request-mail", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id"
+      );
+
+      const subject = "👋 New Connection Request";
+      const body = `
+      <div style="font-family: arial, sans-serif; padding: 20px">
+      <h2>Hi ${connection.to_user_id.full_name},</h2>
+      <p>
+        You have new connection request from
+        ${connection.from_user_id.full_name} -
+        @${connection.from_user_id.username}
+      </p>
+      <p>
+        Click
+        <a href="${process.env.FRONTEND_URL}/connections" style="color: #10b981"
+          >here</a
+        >
+         to accept or reject the request
+      </p>
+      <br />
+      <p>Thanks, <br />PingUp - Stay Connected</p>
+    </div>`;
+
+      await sendEmail({ to: connection.to_user_id.email, subject, body });
+    });
   }
 );
 
